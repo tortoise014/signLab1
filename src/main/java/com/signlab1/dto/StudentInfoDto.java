636d716ab27d14309312a26d8f7d446f9e516114
tortoise <2891138827@qq.com>
2025-09-23 12:51:48 +0800

    fix

---
+++ src/main/java/com/signlab1/dto/StudentInfoDto.java
@@ -0,0 +1,33 @@
+package com.signlab1.dto;
+
+import lombok.Data;
+
+/**
+ * 学生信息DTO
+ */
+@Data
+public class StudentInfoDto {
+    
+    /**
+     * 学生学号
+     */
+    private String studentCode;
+    
+    /**
+     * 学生姓名
+     */
+    private String studentName;
+    
+    /**
+     * 班级编号
+     */
+    private String classCode;
+    
+    /**
+     * 班级名称
+     */
+    private String className;
+}
+
+
+
